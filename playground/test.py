bd931d5cc7d48f6bb2cb10a31b40622739d6b339
Paul <paul.seyffer@outlook.de>
2026-03-23 16:12:38 +0100

    test ty in pre-commit

--- playground/test.py
+++ playground/test.py
@@ -1,6 +1,9 @@
 from sheet_1 import hello
 from sheet_1.pauli_maticies import pauli_x, pauli_y, pauli_z
 
-print(hello())
+
+hello_str = hello()
+
+print(hello_str)
 
 print(f"{pauli_x() @ pauli_y()} = {1j * pauli_z()}")
