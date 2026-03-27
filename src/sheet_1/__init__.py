015e8cb2b37b6520d02973f6b3dbf7ea2750f78d
Alina <akibu06@gmail.com>
2026-03-27 12:19:56 +0100

    init

--- src/sheet_1/__init__.py
+++ src/sheet_1/__init__.py
@@ -1,5 +1,6 @@
 from sheet_1.quantum_simulator import simulate
+from sheet_1.simulator_cuda import simulate_cuda
 from sheet_1.apply_u_on_state import apply_u_on_state
 from sheet_1.apply_cx_on_state import apply_cx_on_state
 
-__all__ = ["apply_cx_on_state", "apply_u_on_state", "simulate"]
+__all__ = ["apply_cx_on_state", "apply_u_on_state", "simulate", "simulate_cuda"]
